f84ce08b3c0c77965d9a0769e7487b1a7304685d
CapisizuCosmin <cosmineala@gmail.com>
2021-12-19 19:25:00 +0200

    Fix: now it works *; Add: on resize; Problem: matrix display differs from the other method
    Problem: cell state is displayed different, potential source: different matrix traversal

--- gameoflife-react/src/components/CTreeJsRenderer.tsx
+++ gameoflife-react/src/components/CTreeJsRenderer.tsx
@@ -1,39 +1,26 @@
 import React from "react";
-import * as THREE from "three"
 import { ICMatrixRenderer } from "../models/Matrix";
 import TreeJsRenderer from "./TreeJsRenderer";
 
-interface ICube extends THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial> {
-    getPositionInMatrix(): {x: number, y: number}
-}
+const CTreeJsRenderer: React.FC<ICMatrixRenderer> = ({ matrix, onCellClickCallback }) => {
 
+    const [treeMatrix, steTreeMatrix] = React.useState<TreeJsRenderer>();
 
-const CTreeJsRenderer: React.FC<ICMatrixRenderer> = ({ matrix, onCellClickCallback}) =>{
-
-    const [ treeMatrix, steTreeMatrix ] = React.useState<TreeJsRenderer>();
-    
-
-    // React.useEffect( () => {
-    //     treeMatrix.update( matrix );
-    // }, [matrix]);
-
-    React.useEffect( () => {
+    React.useEffect(() => {
         steTreeMatrix(
             new TreeJsRenderer({
                 matrix: matrix,
-                canvRoot: document.getElementById("canvasRoot"),
                 clickCallback: onCellClickCallback
             })
         )
-    }, [] );
+    }, []);
 
-    treeMatrix?.update( matrix );
+    treeMatrix?.update();
 
-    return(
+    return (
         <div
-        id="canvasRoot"
+            id="canvasRoot"
         >
-            
         </div>
     );
 };
--- gameoflife-react/src/components/TreeJsRenderer.ts
+++ gameoflife-react/src/components/TreeJsRenderer.ts
@@ -2,18 +2,17 @@ import * as THREE from "three";
 import { IMatrix } from "../models/Matrix";
 
 interface ICube extends THREE.Mesh<THREE.PlaneGeometry, THREE.MeshBasicMaterial> {
-    getPositionInMatrix(): {x: number, y: number}
+    getPositionInMatrix(): { x: number, y: number }
 }
 
 //
 
-interface IConstrArgs{
+interface IConstrArgs {
     matrix: IMatrix,
-    canvRoot: HTMLElement | null,
-    clickCallback( x: number, y: number): void
+    clickCallback(x: number, y: number): void
 }
 
-export default class TreeJsRenderer implements IConstrArgs{
+export default class TreeJsRenderer implements IConstrArgs {
 
     matrix;
     canvRoot;
@@ -27,56 +26,57 @@ export default class TreeJsRenderer implements IConstrArgs{
     group_matrix = new THREE.Group();
 
     material_black = new THREE.MeshBasicMaterial({ color: 0x000000 });  // greenish blue
-    material_white = new THREE.MeshBasicMaterial({ color: 0xffffff }); 
+    material_white = new THREE.MeshBasicMaterial({ color: 0xffffff });
 
-    constructor({ 
+    constructor({
         matrix,
-        canvRoot,
         clickCallback
-    }: IConstrArgs ){
+    }: IConstrArgs) {
 
         this.matrix = matrix;
         this.clickCallback = clickCallback;
 
-        this.renderer.setSize( window.innerWidth, window.innerHeight );
-        
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+
         // render canvas
         this.canvRoot = document.getElementById("canvasRoot");
-        this.canvRoot?.appendChild( this.canvas );
+        this.canvRoot?.appendChild(this.canvas);
 
         // config camera
         const fov = 55;
-        const aspect =  window.innerWidth / window.innerHeight; 
+        const aspect = window.innerWidth / window.innerHeight;
         const near = 0.00001;
         const far = 10_000;
         this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        this.camera.position.set( 0, 0, this.matrix.width );
+        this.camera.position.set(0, 0, this.matrix.width);
 
         this.scene.background = new THREE.Color(0x154575);
 
         this.renderInitialMatrix();
+
         this.configRaycast();
+        this.confOnResize();
+
         this.render();
 
     }
 
-    renderInitialMatrix(){
-        let { width, height, getCell } = this.matrix;
-        
+    renderInitialMatrix() {
+        const { width, height } = this.matrix;
+
         const widthSize = 1.0;
         const heightSize = 1.0;
         const square_geometry = new THREE.PlaneGeometry(widthSize, heightSize);
-        
 
         for (let i = 0; i < width; i++) {
             for (let j = 0; j < height; j++) {
-                // let material = material_black;
-                let material = this.compMaterial( this.matrix.getCell( i,j ) );
-                // let material = ( ( i + j ) % 2 === 0 ) ? this.material_black : this.material_white;
+
+                let material = this.compMaterial( this.matrix.getCell(i, j));
                 let cube = new THREE.Mesh(square_geometry, material) as ICube;
+                // let material = ( ( i + j ) % 2 === 0 ) ? this.material_black : this.material_white;
+
                 cube.position.x = i;
                 cube.position.y = j;
-
                 cube.getPositionInMatrix = () => {
                     return {
                         x: i,
@@ -88,56 +88,63 @@ export default class TreeJsRenderer implements IConstrArgs{
             }
         }
 
-        this.group_matrix.position.set( -width/2, -height/2, 0 );
+        this.group_matrix.position.set(-width / 2, -height / 2, 0);
 
-        this.scene.add( this.group_matrix );
+        this.scene.add(this.group_matrix);
     }
 
-    configRaycast(){
-        
-        var raycaster = new THREE.Raycaster();
-        var mouse = new THREE.Vector2();
+    configRaycast() {
+        const onDocumentMouseDown = (event: any) => {
+            var raycaster = new THREE.Raycaster();
+            var mouse = new THREE.Vector2();
 
-        const onDocumentMouseDown = ( event: any ) => {
+            const { top, left } = this.canvas.getBoundingClientRect();
+            const { clientWidth, clientHeight } = this.renderer.domElement;
 
-            mouse.x = ( (event.clientX - this.canvas.getBoundingClientRect().top ) / this.renderer.domElement.clientWidth ) * 2 - 1;
-            mouse.y = - ( ( event.clientY - this.canvas.getBoundingClientRect().left ) / this.renderer.domElement.clientHeight ) * 2 + 1;
-            raycaster.setFromCamera( mouse, this.camera );
-    
-            var intersects = raycaster.intersectObjects( this.group_matrix.children );
-      
+            mouse.x =   ( ( event.clientX - top )  / clientWidth  ) * 2 - 1;
+            mouse.y = - ( ( event.clientY - left ) / clientHeight ) * 2 + 1;
+            raycaster.setFromCamera(mouse, this.camera);
+
+            var intersects = raycaster.intersectObjects(this.group_matrix.children);
+
             for (let i = 0; i < intersects.length; i++) {
                 let cube = intersects[i].object as ICube;
                 let { x, y } = cube.getPositionInMatrix();
-                this.clickCallback( x,y );
+                this.clickCallback(x, y);
             }
-
-            // this.render();
         }
         document.addEventListener('click', onDocumentMouseDown, false);
-
     }
 
-    render(){
-        this.renderer.render( this.scene, this.camera );
+    confOnResize(){
+        const onDocumentresize = () => {
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+            this.renderer.setSize( window.innerWidth, window.innerHeight );
+            this.render();
+        }
+        window.addEventListener( 'resize', onDocumentresize, false );
     }
 
-    update( matrix: IMatrix ){
+    render() {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    update() {
         let list = this.group_matrix.children;
-        list.forEach( element => {
+        list.forEach(element => {
 
             let cube = element as ICube;
             let { x, y } = cube.getPositionInMatrix();
 
-            let material = this.compMaterial( matrix.getCell( x, y ) );
+            let material = this.compMaterial(this.matrix.getCell(x, y));
             cube.material = material;
 
         });
-        this.matrix = matrix;
         this.render();
     }
 
-    compMaterial( state: boolean ): THREE.MeshBasicMaterial{
+    compMaterial(state: boolean): THREE.MeshBasicMaterial {
         return state ? this.material_white : this.material_black;
     }
 
