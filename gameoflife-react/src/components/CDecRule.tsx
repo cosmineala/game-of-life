66b9e3038a92a16b6d0f8137877947f36429d98c
CapisizuCosmin <cosmineala@gmail.com>
2021-12-25 16:05:33 +0200

    fix: input no longer deselects after input
    Problem: Form field deselected after first input.
    Solution: Give key to each elements of the generated list so React can match old to new component.

--- gameoflife-react/src/components/CDecRule.tsx
+++ gameoflife-react/src/components/CDecRule.tsx
@@ -13,7 +13,7 @@ interface IReducerArgs { // function
     action: {
       type: Action
       args?: {
-        pairObjChild?:{
+        pairObjChild?: {
           obj: any,
           child: string,
           value: any;
@@ -36,8 +36,8 @@ const reducer: IReducerArgs = (state, action) => {
   switch (action.type) {
 
     case Action.changeChildNumber:
-      if( args?.pairObjChild ){
-        let { obj , child, value } = args.pairObjChild;
+      if (args?.pairObjChild) {
+        let { obj, child, value } = args.pairObjChild;
         obj[child] = value;
       }
       break;
@@ -59,8 +59,8 @@ const CDecRule: React.FC<IProps> = ({ ijUser }) => {
 
   const rule = state.user.ijRule as IJRule;
 
-  const changeDispatch = ( parent: object, childStr: string, value: any ) => {
-    dispach( { type: Action.changeChildNumber, args:{ pairObjChild:{ obj: parent, child: childStr, value: value } } } );
+  const changeDispatch = (parent: object, childStr: string, value: any) => {
+    dispach({ type: Action.changeChildNumber, args: { pairObjChild: { obj: parent, child: childStr, value: value } } });
   };
 
   return (
@@ -74,7 +74,7 @@ const CDecRule: React.FC<IProps> = ({ ijUser }) => {
           <input type="number"
             value={rule.nsr}
             // onChange={(event: any) => { dispach({ type: Action.changeChildNumber, args: event.target.value }) }}
-            onChange={(e: any) => { changeDispatch( rule,"nsr", e.target.value ) }}
+            onChange={(e: any) => { changeDispatch(rule, "nsr", e.target.value) }}
           />
         </label>
         <ul className="scenarios">
@@ -85,13 +85,13 @@ const CDecRule: React.FC<IProps> = ({ ijUser }) => {
               const scen = rule.scenarios[i];
 
               scenarios.push(
-                <li>
+                <li key={`Scen[${i}]`} >
 
                   <label htmlFor="">
                     requiredState:
                     <input type="checkbox"
                       checked={scen.requiredState}
-                      onChange={ (e:any)=>{ changeDispatch(scen, "requiredState", e.target.checked) }}
+                      onChange={(e: any) => { changeDispatch(scen, "requiredState", e.target.checked) }}
                     />
                   </label>
 
@@ -99,7 +99,7 @@ const CDecRule: React.FC<IProps> = ({ ijUser }) => {
                     isEnabled:
                     <input type="checkbox"
                       checked={scen.isEnabled}
-                      onChange={ (e:any) => { changeDispatch(scen, "isEnabled", e.target.checked) } }
+                      onChange={(e: any) => { changeDispatch(scen, "isEnabled", e.target.checked) }}
                     />
                   </label>
                   <ul className="requierments">
@@ -109,19 +109,19 @@ const CDecRule: React.FC<IProps> = ({ ijUser }) => {
                         const req = scen.requierments[j];
 
                         req_list.push(
-                          <li key={Math.random()} className="requierment" >
+                          <li key={ `req[${j}]` } className="requierment" >
                             <label htmlFor="">
                               Max:
                               <input type="number"
                                 value={req.max}
-                                onChange={ (e:any)=>{ changeDispatch( req, "max", e.target.value ) } }
+                                onChange={(e: any) => { changeDispatch(req, "max", e.target.value) }}
                               />
                             </label> <br />
                             <label htmlFor="">
                               Min:
                               <input type="number"
                                 value={req.min}
-                                onChange={ (e:any)=>{ changeDispatch( req, "min", e.target.value ) } }
+                                onChange={(e: any) => { changeDispatch(req, "min", e.target.value) }}
                               />
                             </label>
                           </li>
@@ -136,20 +136,20 @@ const CDecRule: React.FC<IProps> = ({ ijUser }) => {
                     ON TRUE | elabled:
                     <input type="checkbox"
                       checked={scen.enableOnTrue}
-                      onChange={ (e:any) => { changeDispatch(scen, "enableOnTrue",e.target.checked) } }
+                      onChange={(e: any) => { changeDispatch(scen, "enableOnTrue", e.target.checked) }}
                     />
                     | value:
-                    <input type="checkbox" 
-                      checked={scen.setOnTrue} 
-                      onChange={ (e:any) => { changeDispatch(scen, "setOnTrue",e.target.checked) } }  
+                    <input type="checkbox"
+                      checked={scen.setOnTrue}
+                      onChange={(e: any) => { changeDispatch(scen, "setOnTrue", e.target.checked) }}
                     />
                   </label><br />
 
                   <label htmlFor="">
                     ON FALSE | elabled:
-                    <input type="checkbox" checked={scen.enableOnFalse} onChange={ (e:any) => { changeDispatch(scen, "enableOnFalse",e.target.checked) } } />
+                    <input type="checkbox" checked={scen.enableOnFalse} onChange={(e: any) => { changeDispatch(scen, "enableOnFalse", e.target.checked) }} />
                     | value
-                    <input type="checkbox" checked={scen.setOnFalse} onChange={ (e:any) => { changeDispatch(scen, "setOnFalse",e.target.checked) } } />
+                    <input type="checkbox" checked={scen.setOnFalse} onChange={(e: any) => { changeDispatch(scen, "setOnFalse", e.target.checked) }} />
                   </label><br />
 
                 </li>
